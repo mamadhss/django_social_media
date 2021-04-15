98ab4c4fffda870546dfb5f83320dcdb9457827c
mamadhss <mamadhss@yahoo.com>
2021-04-16 02:08:58 +0430

    show user posts where there are on your following list

--- App_Posts/views.py
+++ App_Posts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render,HttpResponse
 from django.contrib.auth.models import User
-
+from App_Login.models import Follow
+from .models import Post
 def home(request):
+    following_list = Follow.objects.filter(follower=request.user)
+    posts = Post.objects.filter(author__in=following_list.values_list('following'))
+
+
     
     search = request.GET.get('search')
     users = User.objects.all()
@@ -13,6 +18,7 @@ def home(request):
         'title':'home page',
         'search':search,
         'users':users,
+        'posts':posts
     }
       
 
